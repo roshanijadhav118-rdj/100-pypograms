fbe3309179f08d6bc95564040a9d301e788e51f7
TechWarrior <roshnijadhav118@gmail.com>
2026-07-31 23:00:26 +0530

    Add class to check if numbers are even or odd

---
+++ Class program.py
@@ -0,0 +1,10 @@
+class ample:
+    def check (self,n):
+        if n%2 ==0:
+            print("even")
+        else:
+            print("odd") 
+            return n
+a=ample()   
+a.check(5)
+a.check(16)            
